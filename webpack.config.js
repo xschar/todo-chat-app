48f2d3f084be4bfa4e26cc025403a2dda9d50f97
trshmdo <segovskiy@gmail.com>
2026-02-13 10:33:49 +0300

    build: create empty boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+    mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "..", "dist"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    modules: ["src", "node_modules"]
+  },
+  module: {
+    strictExportPresence: true,
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader", // translates css into CommonJS
+            options: {
+              esModule: true,
+              // css modules
+              modules: {
+                localIdentName: "[name]__[local]__[hash:base64:5]", // format of output
+                namedExport: true // named exports instead of default
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "", "./src/index.html")
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  devServer: {
+    static: "./dist"
+  }
+}
